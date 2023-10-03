7dae88a738492b2c77e7123b56a13e1df9c1de87
Michael Utama <michaelu670@gmail.com>
2023-10-03 17:39:40 +0700

    add icon and appropriate title

--- src/app/component/product/ProductSearchPage.php
+++ src/app/component/product/ProductSearchPage.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touce-icon" sizes="180x180" href="<?= BASE_URL ?>/images/icon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= BASE_URL ?>/images/icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= BASE_URL ?>/images/icon/favicon-16x16.png">
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/product/search-result.css">
     <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/template/global.css">
-    <title>Search results</title> <!-- TODO: add search text -->
+    <title>Search results for <?php $searchStr = isset($_GET['q']) ? $_GET['q'] : ' '; echo $searchStr; ?></title> <!-- TODO: add search text -->
 </head>
 <body>
     <div class="flex-container">
