cc609f2dec8d36cc1c976e1e8220f820a70ef554
Michael Utama <michaelu670@gmail.com>
2023-10-06 18:31:26 +0700

    feat: carry tag at GET method to checkbox

--- src/app/component/product/TagCheckbox.php
+++ src/app/component/product/TagCheckbox.php
@@ -1,9 +1,13 @@
 <?php
 
 function echoTagCheckbox($data) {
+    $checked = '';
+    if (isset($_GET['tags']) && in_array($data, explode(',', $_GET['tags']))) {
+        $checked = 'checked';
+    }
     $html = <<<"EOT"
         <label class="container-checkbox" for="$data">$data
-            <input type="checkbox" id="$data" class="tag-checkbox">
+            <input type="checkbox" id="$data" class="tag-checkbox" $checked>
             <span class="checkmark"></span>
         </label>
         <br>
