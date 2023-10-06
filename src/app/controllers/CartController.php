2e53ec8f5fceb058a6407c0096d5d326d6776ca6
Michael Utama <michaelu670@gmail.com>
2023-10-06 17:28:33 +0700

    fix: now cannot open cart before login

--- src/app/controllers/CartController.php
+++ src/app/controllers/CartController.php
@@ -10,25 +10,30 @@ class CartController extends Controller implements ControllerInterface {
     }
 
     function index() {
-        $userID = $_SESSION['user_id']; 
-        $activeCart = $this->cartModel->getActiveCart($userID);
-
+        if (!isset($_SESSION['user_id'])) {
+            header('Location: /public/user/login');
+            exit();
+        }
         $activeCart['username'] = null;
         $activeCart['picture_url'] = 'user.svg';
 
-        if(isset($_SESSION['user_id']))
-        {
-            require_once __DIR__ . '/../model/UserModel.php';
-            $userModel = new UserModel();
-            $user = $userModel->getUserFromID($_SESSION['user_id']);
-            $activeCart['username'] = $user->username;
-            $activeCart['access_type'] = $user->access_type;
-            $activeCart['picture_url'] = $user->picture_url;
+        require_once __DIR__ . '/../model/UserModel.php';
+        $userModel = new UserModel();
+        $user = $userModel->getUserFromID($_SESSION['user_id']);
 
-        } else
-        {
-            $homeView = $this->view('home', 'MainView', ['username' => null, 'picture_url' => 'user.svg']);
+        // if not user, return not found
+        if ($user->access_type != 0) {
+            header('Location: /public/notfound');
+            exit();
         }
+
+        $activeCart['username'] = $user->username;
+        $activeCart['access_type'] = $user->access_type;
+        $activeCart['picture_url'] = $user->picture_url;
+
+
+        $userID = $_SESSION['user_id']; 
+        $activeCart = $this->cartModel->getActiveCart($userID);
         
         foreach ($activeCart['products'] as &$product) {
             $product = array_merge($product, $this->productModel->getProductFromID($product['product_id']));
