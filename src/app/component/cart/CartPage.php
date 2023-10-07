4b0fb6cdd6565ef68a79088af05b1acd5ffc1708
Michaelu670 <michaelu670@gmail.com>
2023-10-07 22:02:12 +0700

    add missing page icon & title

--- src/app/component/cart/CartPage.php
+++ src/app/component/cart/CartPage.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touce-icon" sizes="180x180" href="<?= BASE_URL ?>/images/icon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= BASE_URL ?>/images/icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= BASE_URL ?>/images/icon/favicon-16x16.png">
     <!-- Global CSS -->
     <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/template/global.css">
     <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/cart/cart.css">
@@ -15,7 +18,7 @@
         const CSRF_TOKEN = "<?= $_SESSION['csrf_token'] ?? '' ?>";
     </script>
     <script type="text/javascript" src="<?= BASE_URL ?>/javascript/component/navbar.js" defer></script>
-    <title>Document</title>
+    <title>Cart</title>
 </head>
 <body>
     <div class="white-body">
