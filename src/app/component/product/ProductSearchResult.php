7309b372f68b4d50b46f99b1736501ad56bb33d0
Michael Utama <michaelu670@gmail.com>
2023-10-09 12:41:10 +0700

    fix: output non-product data

--- src/app/component/product/ProductSearchResult.php
+++ src/app/component/product/ProductSearchResult.php
@@ -1,6 +1,9 @@
 <div class="flex-container">
     <?php require_once 'ProductCard.php' ?>
     <?php foreach($this->data as $product) {
+        if (!isset($product['product_id'])) {
+            continue;
+        }
         if($this->access_type == 0) {echoProductCard($product);}
         else {echoAdminProductCard($product);}
         
